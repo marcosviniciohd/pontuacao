ea50d73a5dea043670d97eabc1166bc6cf89a5db
Marcos Vinício <marcosviniciohd@hotmail.com>
2020-05-15 19:25:36 -0300

    Projeto criado

---
+++ src/br/com/marcosfernandes/Pontuacao.java
@@ -0,0 +1,24 @@
+package br.com.marcosfernandes;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Pontuacao {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+	}
+
+}
