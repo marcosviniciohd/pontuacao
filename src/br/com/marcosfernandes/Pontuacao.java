f960f2602dc8c73faf9882d5e728e54959f23fd7
Marcos Vinício <marcosviniciohd@hotmail.com>
2020-05-15 19:31:22 -0300

    Cabecalho

--- src/br/com/marcosfernandes/Pontuacao.java
+++ src/br/com/marcosfernandes/Pontuacao.java
@@ -10,7 +10,9 @@ public class Pontuacao {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("-------------------------------- ");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO ");
 		
 		
 		
